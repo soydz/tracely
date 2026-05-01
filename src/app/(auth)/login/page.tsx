dcd908627b31c6f12a459398960fd55add3374a0
soydz <dev@soydz.com>
2026-05-01 07:32:36 -0500

    fix(pages): add missing titles

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,11 @@ import { LoginForm } from "@/features/auth/components/LoginForm";
 export default function Login() {
   return (
     <div className="h-svh flex justify-center items-center ">
-      <div className="w-md">
+      <div className="w-md flex flex-col gap-10">
+        <div className="text-center">
+          <h2 className="text-4xl md:text-5xl font-serif text-foreground tracking-tighter leading-tight">Secure Access</h2>
+          <p className="text-muted text-sm md:text-base mx-auto leading-relaxed mt-2">Return to your dashboard to manage your wealth and budgets</p>
+        </div>
         <LoginForm />
       </div>
     </div>
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,11 @@ import { RegisterForm } from "@/features/auth/components/RegisterForm";
 export default function register() {
   return (
     <div className="h-svh flex justify-center items-center ">
-      <div className="w-md">
+      <div className="w-md flex flex-col gap-10">
+        <div className="text-center">
+          <h2 className="text-4xl md:text-5xl font-serif text-foreground tracking-tighter leading-tight">Join Tracely</h2>
+          <p className="text-muted text-sm md:text-base mx-auto leading-relaxed mt-2">Experience a refined way to track spending and master your savings</p>
+        </div>
         <RegisterForm />
       </div>
     </div>
